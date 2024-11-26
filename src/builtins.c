3b4bca961737540096405d109e057b4190696ece
pf4 <74263128+elydre@users.noreply.github.com>
2024-11-26 17:07:22 +0100

    patch buildtins.h

--- src/builtins.c
+++ src/builtins.c
@@ -41,49 +41,49 @@ int testcmd(int, char **);
 int killcmd(int, char **);
 
 const struct builtincmd builtincmd[] = {
-	{ ".", dotcmd, 3 },
-	{ ":", truecmd, 3 },
-	{ "[", testcmd, 0 },
-	{ "alias", aliascmd, 6 },
+	{ ".", dotcmd, 3 },         // 0
+	{ ":", truecmd, 3 },        // 1
+	{ "[", testcmd, 0 },        // 2
+	{ "alias", aliascmd, 6 },   // 3
 #if JOBS
 	{ "bg", bgcmd, 2 },
 #endif
-	{ "break", breakcmd, 3 },
-	{ "cd", cdcmd, 2 },
-	{ "chdir", cdcmd, 0 },
-	{ "command", commandcmd, 2 },
-	{ "continue", breakcmd, 3 },
-	{ "echo", echocmd, 0 },
-	{ "eval", NULL, 3 },
-	{ "exec", execcmd, 3 },
-	{ "exit", exitcmd, 3 },
-	{ "export", exportcmd, 7 },
-	{ "false", falsecmd, 2 },
+	{ "break", breakcmd, 3 },   // 4
+	{ "cd", cdcmd, 2 },         // 5
+	{ "chdir", cdcmd, 0 },      // 6
+	{ "command", commandcmd, 2 }, // 7
+	{ "continue", breakcmd, 3 }, // 8
+	{ "echo", echocmd, 0 },     // 9
+	{ "eval", NULL, 3 },        // 10
+	{ "exec", execcmd, 3 },     // 11
+	{ "exit", exitcmd, 3 },     // 12
+	{ "export", exportcmd, 7 }, // 13
+	{ "false", falsecmd, 2 },   // 14
 #if JOBS
 	{ "fg", fgcmd, 2 },
 #endif
-	{ "getopts", getoptscmd, 2 },
-	{ "hash", hashcmd, 2 },
-	{ "jobs", jobscmd, 2 },
-	{ "kill", killcmd, 2 },
-	{ "local", localcmd, 7 },
-	{ "printf", printfcmd, 0 },
-	{ "pwd", pwdcmd, 2 },
-	{ "read", readcmd, 2 },
-	{ "readonly", exportcmd, 7 },
-	{ "return", returncmd, 3 },
-	{ "set", setcmd, 3 },
-	{ "shift", shiftcmd, 3 },
-	{ "test", testcmd, 0 },
-	{ "times", timescmd, 3 },
-	{ "trap", trapcmd, 3 },
-	{ "true", truecmd, 2 },
-	{ "type", typecmd, 2 },
+	{ "getopts", getoptscmd, 2 }, // 15
+	{ "hash", hashcmd, 2 },     // 16
+	{ "jobs", jobscmd, 2 },     // 17
+	{ "kill", killcmd, 2 },     // 18
+	{ "local", localcmd, 7 },   // 19
+	{ "printf", printfcmd, 0 }, // 20
+	{ "pwd", pwdcmd, 2 },       // 21
+	{ "read", readcmd, 2 },     // 22
+	{ "readonly", exportcmd, 7 }, // 23
+	{ "return", returncmd, 3 }, // 24
+	{ "set", setcmd, 3 },       // 25
+	{ "shift", shiftcmd, 3 },   // 26
+	{ "test", testcmd, 0 },     // 27
+	{ "times", timescmd, 3 },   // 28
+	{ "trap", trapcmd, 3 },     // 29
+	{ "true", truecmd, 2 },     // 30
+	{ "type", typecmd, 2 },     // 31
 #ifdef HAVE_GETRLIMIT
 	{ "ulimit", ulimitcmd, 2 },
 #endif
-	{ "umask", umaskcmd, 2 },
-	{ "unalias", unaliascmd, 2 },
-	{ "unset", unsetcmd, 3 },
-	{ "wait", waitcmd, 2 },
+	{ "umask", umaskcmd, 2 },   // 32
+	{ "unalias", unaliascmd, 2 }, // 33
+	{ "unset", unsetcmd, 3 },   // 34
+	{ "wait", waitcmd, 2 },     // 35
 };
